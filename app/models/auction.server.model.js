8fa3288dacd3c68cffbb24a05210617a2bd395e3
Sam Boyles <sbo49@uclive.ac.nz>
2018-03-20 09:42:06 +1300

    Started moving auctions

--- app/models/auction.server.model.js
+++ app/models/auction.server.model.js
@@ -1,233 +1,20 @@
 const db = require('../../config/db');
 const path = require('path');
 const fs = require('fs');
-const crypto = require('crypto');
-const auth = require('../../config/auth.js');
 
-const reset_database = path.join(__dirname, '../../database/create_database.sql');
-const sql_data = path.join(__dirname, '../../database/sql_data.sql');
 const picturePath = path.join(__dirname, '../../uploads');
-
-exports.createUser = function(values, done) {
-    db.get_pool().query('INSERT INTO auction_user ' +
-        '(user_username, user_givenname, user_familyname, user_email, user_password) ' +
-        'VALUES (?, ?, ?, ?, ?)', values,
-        function(err, result) {
-            if(err) return done(400);
-            done(result);
-        });
-};
-
-exports.getUser = function(id, token, done) {
-
-    let loginQuery = "SELECT user_id FROM auction_user WHERE user_token = ?";
-    db.get_pool().query(loginQuery, token, function(err, rowss) {
-        if (err || rowss.length === 0) {
-
-            db.get_pool().query('SELECT * FROM auction_user WHERE user_id = ?', id,
-                function (err, rows) {
-                    if (err) return done(404);
-                    if (rows.length > 0) {
-
-
-                        return done({
-                            "username": rows[0].user_username,
-                            "givenName": rows[0].user_givenname,
-                            "familyName": rows[0].user_familyname
-                        });
-                    } else {
-                        return done(404);
-                    }
-                });
-
-        } else {
-
-            let userId = rowss[0].user_id;
-
-            if(userId.toString() === id) {
-
-                db.get_pool().query('SELECT * FROM auction_user WHERE user_id = ?', id,
-                    function (err, rows) {
-                        if (err) return done(404);
-                        if (rows.length > 0) {
-
-
-                            return done({
-                                "username": rows[0].user_username,
-                                "givenName": rows[0].user_givenname,
-                                "familyName": rows[0].user_familyname,
-                                "email": rows[0].user_email,
-                                "accountBalance": rows[0].user_accountbalance
-                            });
-                        } else {
-                            return done(404);
-                        }
-                    });
-
-            } else {
-                db.get_pool().query('SELECT * FROM auction_user WHERE user_id = ?', id,
-                    function (err, rows) {
-                        if (err) return done(404);
-                        if (rows.length > 0) {
-
-
-                            return done({
-                                "userName": rows[0].user_username,
-                                "givenName": rows[0].user_givenname,
-                                "familyName": rows[0].user_familyname
-                            });
-                        } else {
-                            return done(404);
-                        }
-                    });
-
-
-
-
-
-            }
-        }
-    })
-};
-
-exports.updateUser = function(id, values, done) {
-    let query = "UPDATE auction_user SET " + values + " WHERE user_id = ?";
-    db.get_pool().query(query, id, function(err, rows){
-        if(err) return done(401);
-        if (rows.affectedRows === 1){
-            done(201)
-        } else{
-            done(401);
-        }
-
-    });
-};
-/*
-Logs the user into the website
-@param auth The email/username supplied by the user
-@param pass The password supplied by the user
-@param type Defines whether a username or email has been provided. 1 = Username, 2 = Email
-*/
-exports.userLogin = function(auth, pass, type, done) {
-    /*
-    if (type === 1) {
-        let query = 'SELECT * FROM auction_user where user_username = ? and user_password = ?';
-    } else if (type === 2) {
-        let query = 'SELECT * FROM auction_user where user_email = ? and user_password = ?';
-    }
-
-    db.get_pool().query(query, [auth, pass], function (err, rows) {
-        if (err) {
-            return done({"ERROR": "Invalid username/email/password supplied"});
-        }
-        ;
-
-        if (rows.length > 0) {
-            let userid = rows[0].user_id;
-            let token = rows[0].user_token;
-
-            done({
-                "id": userid,
-                "token": token
-            });
-        } else {
-            return done({"ERROR": "Invalid username/email/password supplied"});
-        }
-        ;
-
-
-    });
-};
-*/ //Trying to make this more compact
-
-    if (type === 1) {
-        db.get_pool().query('SELECT * FROM auction_user WHERE user_username = ? and user_password = ?', [auth, pass], function(err, rows) {
-            if (err) {
-                return done(400);
-            };
-
-            if (rows.length > 0) {
-                let userid = rows[0].user_id;
-                // Generate unique user token for session and place in database
-                let token = crypto.randomBytes(32 * (3 / 4)).toString("base64");
-                db.get_pool().query('UPDATE auction_user SET user_token = ? WHERE user_id = ?', [token, userid]);
-                done({
-                    "id": userid,
-                    "token": token
-                });
-            } else return done(400);
-        });
-
-    } else if (type === 2) {
-        db.get_pool().query('SELECT * FROM auction_user WHERE user_email = ? and user_password = ?', [auth, pass], function(err, rows) {
-            if (err) {
-                return done(400);
-            }
-
-            if (rows.length > 0) {
-                let userid = rows[0].user_id;
-                //Generate user token for session and place in database
-                let token = crypto.randomBytes(32 * (3 / 4)).toString("base64");
-                db.get_pool().query('UPDATE auction_user SET user_token = ? WHERE user_id = ?', [token, userid]);
-                done({
-                    "id": userid,
-                    "token": token
-                });
-            } else return done(400);
-        });
-    }
-};
-
-exports.userLogout = function(token, done) {
-
-    let query = "UPDATE auction_user SET user_token = NULL WHERE user_token = ?";
-    db.get_pool().query(query, token, function(err, rows) {
-        if (err) return done(401);
-        if (rows.affectedRows === 1){
-            done(200)
-        } else {
-            done(401);
-        }
-    });
-};
-
-exports.reset_server = function(done) {
-    fs.readFile(reset_database, {encoding: 'utf-8'}, function (err, data) {
-        if (!err) {
-            db.get_pool().query(data, function(err, rows){
-                if(err) return done(500);
-                done(200);
-            });
-        } else {
-            done(400);
-        }
-    });
-};
-
-exports.repopulate_db = function(done) {
-    fs.readFile(sql_data, {encoding: 'utf-8'}, function (err, data) {
-        if (!err) {
-            db.get_pool().query(data, function (err, rows) {
-                if (err) return done(500);
-                done(201);
-            });
-        } else {
-            return done(400);
-        }
-    });
-};
 
 exports.createAuction = function(values, done) {
 
     db.get_pool().query("INSERT INTO auction " +
         "(auction_categoryid, auction_title, auction_description, auction_startingdate, auction_endingdate, auction_reserveprice, auction_startingprice, auction_userid) " +
         "VALUES (?, ?, ?, ?, ?, ?, ?, ?)", values, function(err, result) {
-            if(err) return done(500);
-            let auction_id = result.insertId;
-            done({
-                "id":auction_id
-            });
+        if(err) return done(500);
+        let auction_id = result.insertId;
+        done({
+            "id":auction_id
         });
+    });
 };
 
 exports.updateAuction = function(id, values, done) {
@@ -255,7 +42,9 @@ exports.updateAuction = function(id, values, done) {
 
 
 };
+//TODO updated api for get auctions, rewrite
 
+//TODO add current bid
 exports.getAuctions = function(startIndex, count, q, category_id, seller, bidder, winner, done) {
     let firstParam = false;
     let query = "SELECT auction.auction_id AS id, category.category_title AS categoryTitle, auction.auction_categoryid AS categoryId, auction.auction_title AS title, " +
@@ -315,20 +104,20 @@ exports.getAuctions = function(startIndex, count, q, category_id, seller, bidder
     }
 
     db.get_pool().query(query, function(err, rows){
-            if(err) {
+        if(err) {
 
-                return done(500);
-            }
+            return done(500);
+        }
 
-            done(rows);
-        });
+        done(rows);
+    });
 };
 
 //TODO 400 bad req
 exports.getOneAuction = function(auctionId ,done) {
 
     let query = "SELECT auction.auction_categoryid AS categoryId, category.category_title AS categoryTitle, auction.auction_title AS title, " +
-    "auction.auction_reserveprice AS reservePrice, auction.auction_startingdate AS startDateTime, auction.auction_endingdate AS endDateTime, " +
+        "auction.auction_reserveprice AS reservePrice, auction.auction_startingdate AS startDateTime, auction.auction_endingdate AS endDateTime, " +
         "auction.auction_description AS description, auction.auction_creationdate AS creationDateTime, auction.auction_primaryphoto_URI AS photoUris," +
         " auction.auction_userid AS id, auction_user.user_username AS username, MAX(bid.bid_amount) AS currentBid, bid.bid_amount AS amount," +
         " bid.bid_datetime AS datetime, bid.bid_userid AS buyerId, auction_user.user_username AS buyerUsername " +
@@ -432,6 +221,7 @@ exports.placeBid = function(amount, id, token, done) {
 
 
 };
+
 
 exports.getPhoto = function(auctionId, done) {
 
